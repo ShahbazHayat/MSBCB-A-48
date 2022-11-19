a9738915292d3d65e86f691f0c060bb65490d6e6
shahbaz hayat <shahbazhayat97@gmail.com>
2022-11-19 13:44:36 +0500

    new file:   adduser.html 	new file:   bs5.html 	new file:   img/avatar 2.png 	new file:   img/avatar10.jpg 	new file:   img/avatar11.png 	new file:   img/avatar3.png 	new file:   img/avatar4.png 	new file:   img/avatar5.png 	new file:   img/avatar6.jpg 	new file:   img/avatar7.png 	new file:   img/avatar8.png 	new file:   img/avatar9.png 	new file:   img/bitsol.jfif 	new file:   img/black.png 	new file:   img/coding1.png 	new file:   img/coding2.jpg 	new file:   img/img1.jpg 	new file:   img/lower.jpg 	new file:   img/qr.jpg 	new file:   index.html 	new file:   mix.html 	new file:   script.js 	new file:   standee.html 	new file:   triangle.html 	new file:   week 4/week4.html 	new file:   week 4/week4.js

---
+++ Class-Work/script.js
@@ -0,0 +1,80 @@
+// task 1
+
+/* var arry1 = ['a','b' ]
+var arry2 = ['c','d' ]
+console.log(arry1.concat(arry2)) */
+
+
+
+// task 2
+
+/* const fruits = ["banana","orange","apple","mango"]
+const iterator1=fruits.entries();
+for(var e of iterator1){
+    console.log(e);
+} */
+
+
+// task 3
+
+/* const obj={a:5,b:7,c:9};
+for(const[key,value]of Object.entries(obj)){
+    console.log('key,value');
+} */
+
+
+// task 4
+
+/* const num =['1','2','3','4','5','6','7','8','9'];
+const even =num.filter(evenval => evenval %2 == 0);
+const odd=num.filter(oddval => oddval  %2 != 0);
+console.log(even , odd);
+ */
+
+// task 5
+/* const val =['ali','shahabz','adil','touseef','aqib','ibrahim'];
+const a =val.filter(starte => starte.startsWith('a'));
+console.log(a); */
+
+
+//task6
+
+/* const arry1=[1,4,9,16]
+const map1=arry1.map(x=>x*x)
+console.log(map1); */
+
+
+//task 7
+/* const array1=['a','2','c']
+array1.forEach(element=>console.log(array1)) */
+
+
+//task8
+/* const array1 =[1,2,3,4];
+function a(p,c)
+{
+    console.log(p,c);
+    return p+c;
+}
+console.log(array1.reduce(a)); */
+
+
+//task9
+/* const array1 =[1,2,3,4];
+function a(p,c)
+{
+    console.log(p,c);
+    return p/c;
+}
+console.log(array1.reduce(a)); */
+
+
+//task10
+/* var arr=['a',['b',['c','d','e']]]
+
+const flatnumbers=arr.flat(Infinity);
+
+console.log(flatnumbers) */
+
+
+
---
+++ Class-Work/week 4/week4.js
@@ -0,0 +1,62 @@
+/* function multiply(a=0,b=1)
+{
+    return a*b;
+    
+}
+
+multiply();
+multiply(5);
+multiply(5,5); */
+
+/* 
+function fun(arg1, ...args)
+{
+    return args.length + 1;
+
+}
+
+console.log(fun(0,1,2,3,4,5,6,7,8,9) ="arguments passed "); */
+
+
+/* const sum = (a, b)=>a+b;
+console.log(sum(2, 3)); */
+
+
+
+
+/* const regExpStr= 'Hellow world ! Hello there';
+const regExpLiteral = /Hello/gi;
+console.log(regExpStr.match(regExpLiteral)); */
+
+
+
+/* 
+const regExpConstructor = new RegExp ('xyz','g');
+
+const str= 'xyz xyz';
+console.log(str.match(regExpConstructor)); */
+
+
+/* const Pattern = prompt('Enter a Pattern');
+const regExpConst = new RegExp (`${Pattern}` , 'gi');
+
+const str = prompt('Enter a Pattern');
+alert(str.match(regExpConst)); */
+
+/* const myRe = /db+d/g
+const Array=myRe.exec( 'cdbbdsbz' )
+
+console.log(Array) */
+
+
+/* 
+function sum(a) {
+  return function (b) {
+    return function (c) {
+      return a + b + c;
+    };
+  };
+}
+
+console.log(sum(1)(2)(3));
+ */
