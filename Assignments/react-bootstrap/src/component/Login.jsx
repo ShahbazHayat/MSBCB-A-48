79f8d42be6c8d4757a8174545446bc93dec0cad7
shahbaz hayat <shahbazhayat97@gmail.com>
2022-11-20 16:36:51 +0500

    Changes to be committed: 	new file:   Assignments/react-bootstrap/.gitignore 	new file:   Assignments/react-bootstrap/README.md 	new file:   Assignments/react-bootstrap/package-lock.json 	new file:   Assignments/react-bootstrap/package.json 	new file:   Assignments/react-bootstrap/public/favicon.ico 	new file:   Assignments/react-bootstrap/public/index.html 	new file:   Assignments/react-bootstrap/public/logo192.png 	new file:   Assignments/react-bootstrap/public/logo512.png 	new file:   Assignments/react-bootstrap/public/manifest.json 	new file:   Assignments/react-bootstrap/public/robots.txt 	new file:   Assignments/react-bootstrap/src/App.css 	new file:   Assignments/react-bootstrap/src/App.js 	new file:   Assignments/react-bootstrap/src/App.test.js 	new file:   Assignments/react-bootstrap/src/component/Counter.jsx 	new file:   Assignments/react-bootstrap/src/component/Login.jsx 	new file:   Assignments/react-bootstrap/src/component/Style.css 	new file:   Assignments/react-bootstrap/src/component/counters.jsx 	new file:   Assignments/react-bootstrap/src/index.css 	new file:   Assignments/react-bootstrap/src/index.js 	new file:   Assignments/react-bootstrap/src/logo.svg 	new file:   Assignments/react-bootstrap/src/reportWebVitals.js 	new file:   Assignments/react-bootstrap/src/setupTests.js 	modified:   Class-Work/React Js/first-project/src/App.js 	new file:   Class-Work/React Js/first-project/src/component/counters.jsx

---
+++ Assignments/react-bootstrap/src/component/Login.jsx
@@ -0,0 +1,60 @@
+//  import React from "react";
+import React, { useRef } from "react";
+// un controlled 
+//  export default function LoginForm() {
+//     const nameField = useRef();
+//     const handleSubmit = (event)=> {
+//         event.preventDefault();
+//         console.log("AGAIN SUBMITTED !");
+//         console.log("nameField: ",nameField.current);
+//         console.log("nameField-value : ",nameField.current.value);
+//     };
+
+//  return(
+//     <div>
+//         <h1>LOGIN FORM</h1>
+//         <form onSubmit={handleSubmit}>
+//             <label for="username">User Name </label>
+//             <input type="text" ref={nameField} placeholder="Enter your Name" />
+            
+//             <label for="password">Password </label>
+//             <input type="password" placeholder="Enter your Password" />
+//             <button>Submit</button>
+//         </form>
+//     </div>
+//  );
+//  }
+
+//controlled
+
+export default function LoginForm() {
+    const [nameField, setNameField] = useState("");
+    const handleSubmit = (event)=> {
+       console.log("event.target.value : ",event.target.name);
+       if (event.target.name === "nameField"){
+        //check name
+       }
+       else{
+
+       }
+       setNameField(event.target.value);
+    };
+
+    
+
+
+ return(
+    <div>
+        <h1>LOGIN FORM</h1>
+        <form onSubmit={handleSubmit}>
+            <label for="username">User Name </label>
+            <input type="text" ref={nameField} placeholder="Enter your Name" />
+            
+            <label for="password">Password </label>
+            <input type="password" placeholder="Enter your Password" />
+            <button>Submit</button>
+        </form>
+    </div>
+ );
+ }
+
