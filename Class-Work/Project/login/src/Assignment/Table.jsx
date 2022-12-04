ad0d6643359e648e01a767509f2b47b6b264c5d6
shahbaz hayat <shahbazhayat97@gmail.com>
2022-12-04 11:38:21 +0500

    new file:   Class-Work/Project/login/src/Assignment/Table.jsx

---
+++ Class-Work/Project/login/src/Assignment/Table.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useEffect, useState } from "react"
+
+const Table = () => {
+    const [users, setUsers] = useState([
+        {id:1, name: "Ram", email:"ram@gmail.com",age:23},
+        {id:1, name: "Ram", email:"ram@gmail.com",age:23},
+        {id:1, name: "Ram", email:"ram@gmail.com",age:23},
+        {id:1, name: "Ram", email:"ram@gmail.com",age:23},
+    ])
+}
+
+const handleDeleteRow = (id)=> {
+    const newUsers = users.filter((x) => x.id !== id);
+    setUsers(newUsers);
+    console.log("users",users);
+};
+
+return(
+    <div className="container">
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {user.length <= 0 && (
+                    <tr>
+                        <td colSpan="5">
+                        <span>no record found </span>
+                        </td>
+                    </tr>
+                )}
+                {user.length != 0 &&
+                users.map((user) => (
+                    <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.age}</td>
+                        <td>{user.email}</td>
+                        <td>
+                            <button></button>
+                        </td>
+                    </tr>
+                ))
+                }
+            </tbody>
+        </table>
+    </div>
+)
+ 
+export deconst [users, setUsers] = useState({
+    id:1, name: "Ram", email:"ram@gmail.com",age:23
+})
